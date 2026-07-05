f9b64b9698a7d0f3ca417e4822d9117348539fc6
Thrishantha N S <thrishantha.23cse@cambridge.edu.in>
2026-07-05 12:01:21 +0530

    Time: 52 ms (61.6%), Space: 85 MB (47.71%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,51 +1,46 @@
 class Solution {
 public:
-vector<vector<string>>ans;
-struct trie{
-    trie* children[26];
-    vector<int>suggestion;
-    trie(){
-        memset(children,0,sizeof(children));
-    }
-};
-trie* root=new trie(); 
-void insert(const string &word,int index){
-    trie* node=root;
-    for(char c:word){
-        int idx=c-'a';
-        if(node->children[idx]==nullptr){
-            node->children[idx]=new trie();
+    vector<vector<string>> ans;
+    struct trie {
+        trie* children[26];
+        vector<int> suggestion;
+        trie() { memset(children, 0, sizeof(children)); }
+    };
+    trie* root = new trie();
+    void insert(const string& word, int index) {
+        trie* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr) {
+                node->children[idx] = new trie();
+            }
+            node = node->children[idx];
+            if (node->suggestion.size() < 3)
+                node->suggestion.push_back(index);
         }
-        node=node->children[idx];
-        if(node->suggestion.size()<3)   node->suggestion.push_back(index);
-
     }
-}
-void search(const string &word,vector<string>& products){
-    trie* node=root;
-    for(char c:word){
-        int idx=c-'a';
-        if(node)    node=node->children[idx];
-        if(node){
-            vector<string>res;
-            for(int i:node->suggestion){
-                res.push_back(products[i]);
+    void search(const string& word, vector<string>& products) {
+        trie* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (node)
+                node = node->children[idx];
+            vector<string> res;
+            if (node) {
+                for (int i : node->suggestion) {
+                    res.push_back(products[i]);
+                }
             }
             ans.push_back(res);
         }
-        else{
-            ans.push_back({});
-        }
     }
-    
-}
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(),products.end());
-        for(int i=0;i<products.size();i++){
-            insert(products[i],i);
+    vector<vector<string>> suggestedProducts(vector<string>& products,
+                                             string searchWord) {
+        sort(products.begin(), products.end());
+        for (int i = 0; i < products.size(); i++) {
+            insert(products[i], i);
         }
-        search(searchWord,products);
+        search(searchWord, products);
         return ans;
-
     }
 };
