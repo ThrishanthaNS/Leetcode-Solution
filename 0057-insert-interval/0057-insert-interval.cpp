decda551815f3ce2eafd308fae309ab3cfcf91e0
Thrishantha N S <thrishantha.23cse@cambridge.edu.in>
2026-08-01 11:26:06 +0530

    Time: 3 ms (30.55%), Space: 21.6 MB (95.54%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& ni) {
+        intervals.push_back(ni);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        int s=intervals[0][0],e=intervals[0][1];
+        int i=0;
+        while(i<intervals.size()){
+            i++;
+            while(i<intervals.size() && e>=intervals[i][0]){
+                if(e<intervals[i][1])
+                    e=intervals[i][1];
+                i++;
+            }
+            res.push_back({s,e});
+            if(i<intervals.size())
+                s=intervals[i][0],e=intervals[i][1];
+           }
+        return res;
+        
+    }
+};
