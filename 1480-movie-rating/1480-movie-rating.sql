61079465426a0642b2ee2df3ad6499fd6f5f3025
Thrishantha N S <thrishantha.23cse@cambridge.edu.in>
2026-08-07 13:12:26 +0530

    Time: 1320 ms (73.2%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+(select u.name as results
+from movies m 
+inner join movierating mr
+on m.movie_id=mr.movie_id 
+inner join users u on
+u.user_id = mr.user_id
+group by u.name 
+order by count(u.name) desc, u.name asc
+limit 1 )
+union all
+(select m.title
+from movies m 
+inner join movierating mr
+on m.movie_id=mr.movie_id 
+where mr.created_at between '2020-02-01' and '2020-02-29'
+group by m.title 
+order by avg(mr.rating) desc, title asc
+limit 1) ;
+
+
