6fffedeac660b5d11452d2ffc69bc37c30e3f8a6
Thrishantha N S <thrishantha.23cse@cambridge.edu.in>
2026-08-20 22:09:17 +0530

    Time: 2 ms (91.81%), Space: 55.9 MB (41.88%) - LeetHub

---
+++ 0002-design-twitter/0002-design-twitter.cpp
@@ -0,0 +1,54 @@
+class Twitter {
+public:
+typedef pair<int,int>pi;
+unordered_map<int,unordered_set<int>>following;
+unordered_map<int,vector<pi>>tweets;
+int timer;
+const int k=10;
+    Twitter() {
+        timer=0;
+    }
+    void postTweet(int userId, int tweetId) {
+         tweets[userId].push_back({timer++,tweetId});
+    }
+    
+    vector<int>getNewsFeed(int userId) {
+        priority_queue<pi,vector<pi>,greater<pi>>pq;
+        int n=tweets[userId].size();
+        for(int i=n-1;i>=max(0,n-k);i--){
+            pq.push({tweets[userId][i]});
+        }
+        for(int followee:following[userId]){
+            int n=tweets[followee].size();
+            for(int i=n-1;i>=max(0,n-k);i--){
+                pq.push({tweets[followee][i]});
+                if(pq.size()>k){
+                    pq.pop();
+                }
+            }
+        }
+        vector<int>feed;
+        while(!pq.empty()){
+            feed.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(feed.begin(),feed.end());
+        return feed;
+    }
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
