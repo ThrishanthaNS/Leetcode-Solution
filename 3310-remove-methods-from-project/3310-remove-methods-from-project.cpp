ec17d73caede96aeb333803a7e7387c0aff65a58
Thrishantha N S <thrishantha.23cse@cambridge.edu.in>
2026-08-31 20:08:58 +0530

    Time: 484 ms (14.79%), Space: 366.7 MB (13.2%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&adj,int s,vector<int>& sus){
-        if(sus[s]==1) return;
-        sus[s]=1;
-        for(int x:adj[s]){
-           dfs(adj,x,sus);
-        }
-    }
-
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-        vector<int>sus(n,0),ans;
-        vector<vector<int>>adj(n);
-        bool isremove=true;
-        for(auto &p:invocations){
-            adj[p[0]].push_back(p[1]);
+        vector<int>sus(n,0),res;
+        queue<int>q;
+        q.push(k);
+        vector<vector<int>>adjlist(n);
+        for(auto p:invocations){
+            adjlist[p[0]].push_back(p[1]);
         }
-        dfs(adj,k,sus);
-        for(auto &p:invocations){
-            if(!sus[p[0]] && sus[p[1]]) {
-                isremove=false;
+        while(!q.empty()){
+            int p=q.front();
+            q.pop();
+            if(sus[p]) continue;
+            sus[p]=1;
+            for(int x:adjlist[p]){
+                q.push(x);
+            }
+        }
+        int isremove=1;
+        for(auto p:invocations){
+            if(!sus[p[0]] && sus[p[1]]){
+                isremove=0;
                 break;
             }
         }
-        if(!isremove){
+        if(isremove){
             for(int i=0;i<n;i++){
-                ans.push_back(i);
+                if(!sus[i]) res.push_back(i);
             }
-            return ans;
         }
-        for(int i=0;i<n;i++){
-            if(sus[i]) continue;
-            ans.push_back(i);
+        else{
+            for(int i=0;i<n;i++){
+                res.push_back(i);
+            }
         }
-        return ans;
+        return res;
     }
 };
